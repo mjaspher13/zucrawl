650b89f16bc6e73226baa1ae988bf8220386d40b
Jaspher <jaspher@esixtycorporation.com>
2019-11-24 16:53:57 +0800

    second commit

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const randnum = Math.round(Math.random() * 10);
 const CryptoJS = require("crypto-js");
 const axios = require("axios");
 const qs = require("qs");
+const moment = require("moment");
 
-app.get("/", (req, res) => {
+app.get("/token", (req, res) => {
   var secret = "338736324b07cd82bb8e5ca97a4badb1";
   var data = {
     email: "jaspher@mailnesia.com",
@@ -23,6 +24,43 @@ app.get("/", (req, res) => {
   axios({
     method: "POST",
     url: "https://gloapi.chinabrands.com/v2/user/login",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    data: qs.stringify(payload)
+  })
+    .then(resp => {
+      res.send(resp.data);
+    })
+    .catch(err => console.log(err));
+});
+
+app.get("/categories", (req, res) => {
+  var payload = {
+    token: "95d3e3a3c401461cb165126da7e3a3a7"
+  };
+  axios({
+    method: "POST",
+    url: "https://gloapi.chinabrands.com/v2/category/index",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    data: qs.stringify(payload)
+  })
+    .then(resp => {
+      res.send(resp.data);
+    })
+    .catch(err => console.log(err));
+});
+
+app.get("/products", (req, res) => {
+  var payload = {
+    token: "95d3e3a3c401461cb165126da7e3a3a7",
+    type: 0
+    // date_start: moment().format(),
+    // date_end: moment().format(),
+    //per_page: 50,
+    //page_number: 1
+  };
+  axios({
+    method: "POST",
+    url: " https://gloapi.chinabrands.com/v2/user/inventory",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
     data: qs.stringify(payload)
   })
