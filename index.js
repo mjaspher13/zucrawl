7ee2d9923635771ee48edda57f00a2178ca15969
Jaspher <jaspher@esixtycorporation.com>
2019-11-25 17:36:36 +0800

    async

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const axios = require("axios");
 const qs = require("qs");
 const moment = require("moment");
 
-app.get("/token", (req, res) => {
+function getToken() {
   var secret = "338736324b07cd82bb8e5ca97a4badb1";
   var data = {
     email: "jaspher@mailnesia.com",
@@ -16,58 +16,68 @@ app.get("/token", (req, res) => {
   };
   data = JSON.stringify(data);
   var signature = CryptoJS.MD5(data + secret).toString();
-  console.log(signature);
   var payload = {
     data: data,
     signature: signature
   };
-  axios({
+  return axios({
     method: "POST",
     url: "https://gloapi.chinabrands.com/v2/user/login",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
     data: qs.stringify(payload)
-  })
-    .then(resp => {
-      res.send(resp.data);
-    })
-    .catch(err => console.log(err));
+  });
+}
+
+app.get("/token", (req, res) => {
+  var token;
+  getToken().then(resp => {
+    token = resp.data.msg.token;
+    res.send(token);
+  });
 });
 
 app.get("/categories", (req, res) => {
-  var payload = {
-    token: "95d3e3a3c401461cb165126da7e3a3a7"
-  };
-  axios({
-    method: "POST",
-    url: "https://gloapi.chinabrands.com/v2/category/index",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    data: qs.stringify(payload)
-  })
-    .then(resp => {
-      res.send(resp.data);
+  getToken().then(resp => {
+    token = resp.data.msg.token;
+    var payload = {
+      token: token
+    };
+    axios({
+      method: "POST",
+      url: "https://gloapi.chinabrands.com/v2/category/index",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      data: qs.stringify(payload)
     })
-    .catch(err => console.log(err));
+      .then(resp => {
+        res.send(resp.data);
+      })
+      .catch(err => console.log(err));
+  });
 });
 
 app.get("/products", (req, res) => {
-  var payload = {
-    token: "95d3e3a3c401461cb165126da7e3a3a7",
-    type: 0
-    // date_start: moment().format(),
-    // date_end: moment().format(),
-    //per_page: 50,
-    //page_number: 1
-  };
-  axios({
-    method: "POST",
-    url: " https://gloapi.chinabrands.com/v2/user/inventory",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    data: qs.stringify(payload)
-  })
-    .then(resp => {
-      res.send(resp.data);
+  getToken().then(resp => {
+    token = resp.data.msg.token;
+
+    var payload = {
+      token: token,
+      type: 0
+      // date_start: moment().format(),
+      // date_end: moment().format(),
+      //per_page: 50,
+      //page_number: 1
+    };
+    axios({
+      method: "POST",
+      url: " https://gloapi.chinabrands.com/v2/user/inventory",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      data: qs.stringify(payload)
     })
-    .catch(err => console.log(err));
+      .then(resp => {
+        res.send(resp.data);
+      })
+      .catch(err => console.log(err));
+  });
 });
 
 app.listen(port, () => console.log(`Ziggurat started on port ${port}!`));
