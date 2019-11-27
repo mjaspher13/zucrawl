4ef9a77e24cc4763cec2acedba13c77e67fafd02
Jaspher <jaspher@esixtycorporation.com>
2019-11-27 15:57:28 +0800

    Added shipping endpoint

--- index.js
+++ index.js
@@ -62,14 +62,102 @@ app.get("/products", (req, res) => {
     var payload = {
       token: token,
       type: 0
-      // date_start: moment().format(),
-      // date_end: moment().format(),
-      //per_page: 50,
-      //page_number: 1
     };
     axios({
       method: "POST",
       url: " https://gloapi.chinabrands.com/v2/user/inventory",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      data: qs.stringify(payload)
+    })
+      .then(resp => {
+        res.send(resp.data);
+      })
+      .catch(err => console.log(err));
+  });
+});
+
+app.get("/product/:prodSku", (req, res) => {
+  //res.send( + req.params.prodSku);
+  getToken().then(resp => {
+    token = resp.data.msg.token;
+
+    var payload = {
+      token: token,
+      cat_id: req.params.prodSku
+    };
+    axios({
+      method: "POST",
+      url: " https://gloapi.chinabrands.com/v2/product/search-sku",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      data: qs.stringify(payload)
+    })
+      .then(resp => {
+        res.send(resp.data);
+      })
+      .catch(err => console.log(err));
+  });
+});
+
+app.get("/productinfo/:prodSku", (req, res) => {
+  //res.send( + req.params.prodSku);
+  getToken().then(resp => {
+    token = resp.data.msg.token;
+
+    var payload = {
+      token: token,
+      goods_sn: req.params.prodSku
+    };
+    axios({
+      method: "POST",
+      url: " https://gloapi.chinabrands.com/v2/product/index",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      data: qs.stringify(payload)
+    })
+      .then(resp => {
+        res.send(resp.data);
+      })
+      .catch(err => console.log(err));
+  });
+});
+
+app.get("/getshipping/:prodSku/:qty/:warehouse", (req, res) => {
+  getToken().then(resp => {
+    token = resp.data.msg.token;
+    goods = JSON.stringify([
+      { sku: req.params.prodSku, quantity: req.params.qty }
+    ]);
+
+    var payload = {
+      token: token,
+      country: "PH",
+      warehouse: req.params.warehouse,
+      //shipping: req.params.shipper,
+      goods: goods
+    };
+    axios({
+      method: "POST",
+      url: "https://gloapi.chinabrands.com/v2/shipping/cost",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      data: qs.stringify(payload)
+    })
+      .then(resp => {
+        res.send(resp.data);
+      })
+      .catch(err => console.log(err));
+  });
+});
+
+app.get("/getshippers", (req, res) => {
+  //res.send( + req.params.prodSku);
+  getToken().then(resp => {
+    token = resp.data.msg.token;
+
+    var payload = {
+      token: token
+    };
+    axios({
+      method: "POST",
+      url: "https://gloapi.chinabrands.com/v2/shipping/index",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       data: qs.stringify(payload)
     })
